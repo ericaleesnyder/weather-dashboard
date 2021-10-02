764b831138fec2ac106d5e4048297fdb4749f5e0
Erica Snyder <ericaleesnyder@gmail.com>
2021-10-01 19:29:01 -0700

    added JavaScript to retrieve weather data from API

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,3 +10,35 @@
 // date, an icon representation of weather conditions, the temperature, the wind speed, and the humidity
 // WHEN I click on a city in the search history
 // THEN I am again presented with current and future conditions for that city
+
+// start with section style="display: hidden"
+// submit button sets their display to block and then displays all of the information 
+
+// first step is console log the data from the get api
+// identify the names of the weather icon, temperature, humidity, wind speed, uv index
+
+// Var for submit button
+var submitBtn = document.getElementById("submit");  
+
+// define function for getting weather
+function getWeather (requestWeather) {
+
+  var cityName = document.getElementById("city-name").value;
+  console.log(cityName)
+
+  var requestWeather = "https://api.openweathermap.org/data/2.5/weather?q="+ cityName +"&appid=5c0c4fce484144270b0ea96cd07f8e98"
+  fetch (requestWeather)
+    .then(function(response){
+      return response.json()
+    }) 
+    .then(function(data) {
+      console.log(data);
+    }
+  )
+};
+
+// Event listener initiates getting weather
+submitBtn.addEventListener('click', function (event) {
+  event.preventDefault();
+  getWeather();
+})
